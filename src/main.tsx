5b72598578610599690f5a937e0def524c1689a0
amydojo <amycdo98@gmail.com>
2026-07-13 19:33:24 -0700

    feat: add application-level error containment

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { SpecimenBoundary } from './components/SpecimenBoundary'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SpecimenBoundary name="Interface Behavior Lab">
+      <App />
+    </SpecimenBoundary>
   </StrictMode>,
 )
